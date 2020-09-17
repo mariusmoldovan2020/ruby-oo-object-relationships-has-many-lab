d1c7632d074cabd4d78885b40cee47aff8de7c08
mariusmoldovan <biris_21@yahoo.com>
2020-09-17 12:28:52 -0500

    finish

---
+++ lib/post.rb
@@ -0,0 +1,20 @@
+class Post
+    attr_accessor :author, :title
+@@all = []
+
+    def initialize(title=nil)
+        @title = title
+        @@all << self
+    end
+def self.all
+    @@all
+end
+def author_name
+    if self.author
+      self.author.name
+    else
+      nil
+    end
+  end
+
+end
